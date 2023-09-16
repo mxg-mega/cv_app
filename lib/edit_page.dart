fad4c10092e7a2f025ef074524e2c95891548db1
Muhammad Aliyu Abubakar <muhammadmasanawa75@gmail.com>
2023-09-16 14:20:15 +0100

    Designing the Pre View Page

--- lib/edit_page.dart
+++ lib/edit_page.dart
@@ -5,8 +5,22 @@ import 'package:cv_app/preview_page.dart';
 
 class EditPage extends StatefulWidget {
   final String initialName;
-  final Function(String) updateName;
-  EditPage({super.key, required this.initialName, required this.updateName});
+  final String initialEmail;
+  final String initialSlack;
+  final String initialGithub;
+  final String initialBio;
+
+  Function(String,String,String,String,String) update;
+
+  EditPage({
+    super.key,
+    required this.initialName,
+    required this.initialEmail,
+    required this.initialSlack,
+    required this.initialGithub,
+    required this.initialBio,
+    required this.update
+  });
   @override
   State<EditPage> createState() => _EditPageState();
 }
@@ -15,83 +29,56 @@ class _EditPageState extends State<EditPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   final nameController = TextEditingController();
-  // TextEditingController emailController = TextEditingController();
-  // TextEditingController slackUserNameController = TextEditingController();
-  // TextEditingController gitHubUserNameController = TextEditingController();
-
-  // String fullName = 'Muhammad Aliyu Abubakar';
-  // String email = 'muhammadmasanawa75@gmail.com';
-  // String slack = 'Muhammad Aliyu Abubakar';
-  // String githubUsername = 'mxg-mega';
-
-  // var fullName = 'Muhammad';
-  // var email;
-  // var slack;
-  // var githubUsername;
+  final emailController = TextEditingController();
+  final slackUserNameController = TextEditingController();
+  final gitHubUserNameController = TextEditingController();
+  final bioController = TextEditingController();
 
   @override
   void initState() {
-
-    // emailController.text = 'muhammadmasanawa75@gmail.com';
-    // slackUserNameController.text = 'Muhammad Aliyu Abubakar';
-    // gitHubUserNameController.text = 'mxg-mega';
     nameController.text = widget.initialName;
+    emailController.text = widget.initialEmail;
+    slackUserNameController.text = widget.initialSlack;
+    gitHubUserNameController.text = widget.initialGithub;
+    bioController.text = widget.initialBio;
     super.initState();
-    // nameController.addListener(_syncName);
-    // emailController.addListener(sync);
-    // slackUserNameController.addListener(sync);
-    // gitHubUserNameController.addListener(sync);
   }
-
 
   @override
   void dispose() {
     nameController.dispose();
+    emailController.dispose();
+    slackUserNameController.dispose();
+    gitHubUserNameController.dispose();
+    bioController.dispose();
     super.dispose();
   }
-  // String getName(){
-  //   fullName = nameController.text;
-  //   return fullName;
-  // }
 
-  // List<String> sync(){
-  //
-  //     return info;
-  //
-  // }
+  Container infoField(TextEditingController id, Widget infoIcon, String hint, String label) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: TextFormField(
+          controller: id,
+          decoration: InputDecoration(
+            label: Text(label),
+            hintText: hint,
+            icon: infoIcon,
+          ),
+        ),
+      ),
+    );
+  }
 
-  // TextEditingController infoValue,
-  // controller: infoValue,
-
-  // Container infoField(
-  //     Widget infoIcon, String hint) {
-  //   return Container(
-  //     decoration: BoxDecoration(
-  //         color: Colors.white, borderRadius: BorderRadius.circular(10.0)),
-  //     child: Padding(
-  //       padding: const EdgeInsets.all(10.0),
-  //       child: TextFormField(
-  //         decoration: InputDecoration(
-  //           hintText: hint,
-  //           icon: infoIcon,
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
-  // @override
-  // void dispose() {
-  //   nameController.dispose();
-  //   emailController.dispose();
-  //   slackUserNameController.dispose();
-  //   gitHubUserNameController.dispose();
-  //   super.dispose();
-  // }
-
-  void onSaveChanges(){
-    String editedData = nameController.text;
-    widget.updateName(editedData);
+  void onSaveChanges() {
+    String nameData = nameController.text;
+    String emailData = emailController.text;
+    String slackData = slackUserNameController.text;
+    String gitData = gitHubUserNameController.text;
+    String bioData = bioController.text;
+    widget.update(nameData, emailData, slackData, gitData, bioData);
     Navigator.pop(context);
   }
 
@@ -109,56 +96,70 @@ class _EditPageState extends State<EditPage> {
             key: _formKey,
             child: Column(
               children: [
+                infoField(
+                  nameController,
+                  const Icon(Icons.person),
+                  'Full Name',
+                  'Name',
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                infoField(
+                  emailController,
+                  const Icon(Icons.mail),
+                  'Email Address',
+                  'Email',
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                infoField(
+                  slackUserNameController,
+                  Image.asset(
+                    'assets/icons8-slack-48.png',
+                    scale: 1.5,
+                  ),
+                  'Slack Username',
+                  'Slack Username',
+                ),
+                const SizedBox(
+                  height: 5.0,
+                ),
+                infoField(
+                  gitHubUserNameController,
+                  Image.asset(
+                    'assets/icons8-github-48.png',
+                    scale: 1.5,
+                  ),
+                  'GitHub Username',
+                  'GitHub Username',
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
                 Container(
                   decoration: BoxDecoration(
                       color: Colors.white, borderRadius: BorderRadius.circular(10.0)),
                   child: Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: TextFormField(
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        hintText: 'Full Name',
+                      controller: bioController,
+                      maxLines: 5,
+                      decoration: const InputDecoration(
+                        hintText: 'Bio',
+                        label: Text('Bio'),
                         icon: const Icon(Icons.person),
                       ),
                     ),
                   ),
                 ),
-                // infoField(
-                //     // nameController,
-                //     const Icon(Icons.person),
-                //     'Full Name',
-                // ),
-                // SizedBox(
-                //   height: 5.0,
-                // ),
-                // infoField(
-                //     emailController, const Icon(Icons.mail), 'Email Address', ),
-                // SizedBox(
-                //   height: 5.0,
-                // ),
-                // infoField(
-                //     slackUserNameController,
-                //     Image.asset(
-                //       'assets/icons8-slack-48.png',
-                //       scale: 1.5,
-                //     ),
-                //     'Slack Username'),
-                // SizedBox(
-                //   height: 5.0,
-                // ),
-                // infoField(
-                //     gitHubUserNameController,
-                //     Image.asset(
-                //       'assets/icons8-github-48.png',
-                //       scale: 1.5,
-                //     ),
-                //     'GitHub Username'),
                 SizedBox(
                   height: 5.0,
                 ),
                 FloatingActionButton(
-                  onPressed: (){
-                   onSaveChanges();
+                  onPressed: () {
+                    onSaveChanges();
                   },
                   child: Icon(Icons.save),
                 )
--- lib/preview_page.dart
+++ lib/preview_page.dart
@@ -10,10 +10,18 @@ class PreViewPage extends StatefulWidget {
 
 class _PreViewPageState extends State<PreViewPage> {
   String initialName = 'Muhammad Aliyu Abubakar';
+  String initialEmail = 'muhammadmasanawa75@gmail.com';
+  String initialSlack = 'Raazi';
+  String initialGithub = 'mxg-mega';
+  String initialBio = 'A Passionate developer interested in game, mobile, and web technologies.';
 
-  void syncName(String newName) {
+  void sync(String newName,String newEmail,String newSlack,String newGit,String newBio) {
     setState(() {
       initialName = newName;
+      initialEmail = newEmail;
+      initialSlack = newSlack;
+      initialGithub = newGit;
+      initialBio = newBio;
     });
   }
 
@@ -25,32 +33,54 @@ class _PreViewPageState extends State<PreViewPage> {
         appBar: AppBar(
           title: Text('PreView'),
         ),
-        body: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text('$initialName', style: TextStyle(fontSize: 30),),
+                    SizedBox(height: 5,),
+                    Text('$initialEmail', style: TextStyle(fontSize: 10),),
+                    SizedBox(height: 10,),
+                    Text('Slack Username:', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                    Text('$initialSlack', style: TextStyle(fontSize: 20),),
+                    SizedBox(height: 10,),
+                    Text('Github Username:', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                    Text('$initialGithub', style: TextStyle(fontSize: 20),),
+                    SizedBox(height: 10,),
+                    Text('Bio:', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                    Text('$initialBio', style: TextStyle(fontSize: 20),),
+                  ],
+                ),
               ),
-              child: Column(
-                children: [
-                  Text('$initialName', style: TextStyle(fontSize: 30),),
-                ],
-              ),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => EditPage(
-                        initialName: initialName,
-                        updateName: syncName,
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditPage(
+                          initialName: initialName,
+                          initialEmail: initialEmail,
+                          initialSlack: initialSlack,
+                          initialGithub: initialGithub,
+                          initialBio: initialBio,
+                          update: sync,
+                        ),
                       ),
-                    ),
-                  );
-                },
-                child: Text('Edit'),),
-          ],
+                    );
+                  },
+                  child: Text('Edit'),),
+            ],
+          ),
         ),
       ),
     );
